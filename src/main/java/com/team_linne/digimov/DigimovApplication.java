afdb8b193b1aea54239564c55f2a22232e1d39d2
Calvin Chu <calvinchu26@gmail.com>
2020-12-11 20:28:11 +0800

    first commit

---
+++ src/main/java/com/team_linne/digimov/DigimovApplication.java
@@ -0,0 +1,13 @@
+package com.team_linne.digimov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigimovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigimovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team_linne/digimov/DigimovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_linne.digimov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigimovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

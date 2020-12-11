fe94feb10c23126a515de72b18e73554437d6946
Calvin Chu <calvinchu26@gmail.com>
2020-12-11 22:24:19 +0800

    updated dev, should trigger travis and staging app on heroku

--- src/main/java/com/team_linne/digimov/controller/RootController.java
+++ src/main/java/com/team_linne/digimov/controller/RootController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootController {
     @GetMapping
     public String hello() {
-        return "HelloWorld!";
+        return "HelloWorld from dev:) !";
     }
 }
